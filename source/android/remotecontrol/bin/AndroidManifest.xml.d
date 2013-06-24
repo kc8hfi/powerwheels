83ed15b83576f8baf194d858b97d2e5d439a65e5
Charles Amey <kc8hfi@gmail.com>
2013-06-24 09:56:57 -0400

    forgot to add the files!
    Added in the new AFMotor library,
    the spec sheet for the gps receiver,
    and the android program

---
+++ source/android/remotecontrol/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/kc8hfi/programming/powerwheels/source/android/remotecontrol/bin/AndroidManifest.xml : \
+/home/kc8hfi/programming/powerwheels/source/android/remotecontrol/AndroidManifest.xml \
---
+++ source/android/remotecontrol/bin/remotecontrol-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/home/kc8hfi/programming/powerwheels/source/android/remotecontrol/bin/remotecontrol-debug-unaligned.apk : \
+/home/kc8hfi/programming/powerwheels/source/android/remotecontrol/bin/remotecontrol.ap_ \
+/home/kc8hfi/programming/powerwheels/source/android/remotecontrol/bin/classes.dex \
